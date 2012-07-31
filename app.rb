cf7db391120bf9f1c09f3aa60e7f4a06d8d653b9
weed <weed_7777@yahoo.co.jp>
2012-07-31 11:14:41 +0900

    convert to JSON

--- app.rb
+++ app.rb
@@ -5,5 +5,5 @@ require 'active_support/core_ext'
 
 get '/' do
   xml = Net::HTTP.get 'pipes.yahoo.com', '/pipes/pipe.run?_id=6adb7c2f4644af358fbe273293c80e43&_render=rss&tag=technology'
-  # json = Hash.from_xml(xml).to_json
+  json = Hash.from_xml(xml).to_json
 end
