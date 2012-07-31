220c38e67e5be8812f280fd9271ebc4205e55100
weed <weed_7777@yahoo.co.jp>
2012-07-31 17:47:58 +0900

    added variables host path

--- app.rb
+++ app.rb
@@ -3,8 +3,11 @@ require 'net/http'
 require 'yajl'
 require 'active_support/core_ext'
 
+host = 'pipes.yahoo.com'
+path = '/pipes/pipe.run?_id=6adb7c2f4644af358fbe273293c80e43&_render=rss&tag=technology'
+
 get '/' do
-  xml = Net::HTTP.get 'pipes.yahoo.com', '/pipes/pipe.run?_id=6adb7c2f4644af358fbe273293c80e43&_render=rss&tag=technology'
+  xml = Net::HTTP.get host, path
   xml.force_encoding 'utf-8'
   content_type 'application/json'
   Yajl::Encoder.encode(Hash.from_xml(xml))
