92b0ba03da41132d8fc0ac68e0b80bdb2a171e28
weed <weed_7777@yahoo.co.jp>
2012-07-31 11:31:54 +0900

    added force encoding

--- app.rb
+++ app.rb
@@ -5,5 +5,6 @@ require 'active_support/core_ext'
 
 get '/' do
   xml = Net::HTTP.get 'pipes.yahoo.com', '/pipes/pipe.run?_id=6adb7c2f4644af358fbe273293c80e43&_render=rss&tag=technology'
+  xml.force_encoding 'utf-8'
   json = Hash.from_xml(xml).to_json
 end
