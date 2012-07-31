5cd52a795e83575211d42003691dd881690b2f7d
weed <weed_7777@yahoo.co.jp>
2012-07-31 17:44:28 +0900

    small change

--- app.rb
+++ app.rb
@@ -6,7 +6,6 @@ require 'active_support/core_ext'
 get '/' do
   xml = Net::HTTP.get 'pipes.yahoo.com', '/pipes/pipe.run?_id=6adb7c2f4644af358fbe273293c80e43&_render=rss&tag=technology'
   xml.force_encoding 'utf-8'
-  # json = Hash.from_xml(xml).to_json
   content_type 'application/json'
   Yajl::Encoder.encode(Hash.from_xml(xml))
 end
