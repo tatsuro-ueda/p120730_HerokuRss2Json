01188d610322f0a69c0360f85c12a7b26059ec81
weed <weed_7777@yahoo.co.jp>
2012-07-31 10:41:46 +0900

    RSS to JSON

--- app.rb
+++ app.rb
@@ -1,18 +1,10 @@
-require 'rss'
+# -*- encoding: utf-8 -*-
 require 'sinatra'
+require 'json'
+require 'net/http'
+require 'active_support/core_ext'
 
-helpers do
-  def put_rss
-    rss_source = 'http://pipes.yahoo.com/pipes/pipe.run?_id=6adb7c2f4644af358fbe273293c80e43&_render=rss&tag=technology'
-    rss = RSS::Parser.parse(rss_source, true)
-    text =''
-    rss.channel.items.each do |item|
-      text += item.title
-    end
-    p text
-  end
-end
-    
 get '/' do
-  put_rss
+  xml = Net::HTTP.get 'pipes.yahoo.com', '/pipes/pipe.run?_id=6adb7c2f4644af358fbe273293c80e43&_render=rss&tag=technology'
+  # json = Hash.from_xml(xml).to_json
 end
